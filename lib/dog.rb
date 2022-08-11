4ee0fa5008a6239df9a252400d2a907aae721d73
Abzed <ibraabzed@gmail.com>
2022-08-11 15:46:29 +0300

    commit tree b9a65365c5474a3233048a7849eb1bb4399cb105

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+    attr_accessor :name
+    attr_accessor :breed
+    def name(name, breed)
+        @name = name
+        @breed = breed
+    end
+end
 
+class Person
+    attr_accessor :name
+    attr_accessor :job
+    def name(name, job)
+        @name = name
+        @job = job
+    end
 end
